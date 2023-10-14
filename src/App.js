12be531efdd129ebd781defd443479bd8cf75814
Your Name <you@example.com>
2023-10-14 21:11:41 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "./commonents/Header";
+import Navbar from "./commonents/Navbar";
+import Profile from "./commonents/Profile";
+
+const App = () => {
+    return (
+        <div className='wrapper'>
+        <div className="container">
+          <Header/>
+            <Navbar/>
+           <Profile/>
+
+        </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/commonents/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () =>{
+    return(
+        <nav className="navbar">
+            <ul>
+                <li>Profile</li>
+                <li>Message</li>
+                <li>News</li>
+                <li>Music</li>
+                <li>Settings</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar
